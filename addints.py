5075209e818167df4f8ac47f83d77d1ac7b85519
mahrokh <mahrokhh.ebrahimi@gmail.com>
2020-06-17 18:25:05 -0700

    python step

--- addints.py
+++ addints.py
@@ -1,5 +1,7 @@
 import sys
 import stdio
+'''stdio library couldn't be imported. 
+it is added mauallu by saving into another folder.'''
 
 n = int(sys.argv[1])
 
@@ -8,5 +10,14 @@ for i in range(n):
     total += stdio.readInt()
 
 
-print('the sum is %d' %total )
+print('Sum is %d' %total )
+
+'''output:
+%puthon addints.py 4
+10
+20
+30
+40
+Sum is 100. '''
+
 
---
+++ twentyquestion.py
@@ -0,0 +1,19 @@
+import random
+import stdio
+
+RANGE= 100
+secret = random.randrange(1,RANGE+1)
+print('guess a number between 1 and %d' %RANGE)
+
+guess= 0
+while guess !=secret:
+    print('what is your guess?', end=' ')
+    guess = stdio.readInt()
+
+    if guess < secret: print('too low')
+    elif guess > secret: print('too high')
+    else:
+        print('you win!')
+
+
+
