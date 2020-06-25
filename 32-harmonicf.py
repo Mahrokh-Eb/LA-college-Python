eea3204f15d9510bacf1b8e1dc238a4ff9661860
mahrokh <mahrokhh.ebrahimi@gmail.com>
2020-06-24 17:40:56 -0700

    python step

---
+++ 32-harmonicf.py
@@ -0,0 +1,20 @@
+'''
+Programmer: Mahrokh Ebrahimi
+Discription:
+    python structure programming
+    explaination of the function
+Date: 6/24/2020
+'''
+import sys
+
+def harmonic(n):
+    total = 0
+    for i in range(1, n+1):
+        total += 1/ i
+    return total
+
+for i in range(1, len(sys.argv)):
+    arg = int(sys.argv[i])
+    value = harmonic(arg)
+    print(value)
+
