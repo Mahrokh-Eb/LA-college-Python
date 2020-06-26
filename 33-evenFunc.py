cd3e4a1a4ae7ea51b4409a12455967ea553f7722
mahrokh <mahrokhh.ebrahimi@gmail.com>
2020-06-26 15:54:37 -0700

    python step

--- 33-evenFunc.py
+++ 33-evenFunc.py
@@ -1,20 +1,20 @@
 '''
 Programmer: Mahrokh Ebrahimi
-Discription: check if a digit is prime
+Discription: check if a digit is odd or even
 Date: 6/25/2020
 '''
 import sys
 
-def prime(n):
+def even(n):
     if n <= 2: return False
     digit = n
     if digit % 2 == 0:
-        print('it is prime')
+        print('it is even')
     elif digit % 2 !=0:
-        print(' it is NOT prime!')
+        print(' it is NOT odd!')
     return digit
 
-print(prime(int(sys.argv[1])))
+print(even(int(sys.argv[1])))
 
 
 
