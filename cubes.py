1c8936e0a9717ae2342bc14c1655064c474cd6d6
mahrokh <mahrokhh.ebrahimi@gmail.com>
2020-06-26 20:20:10 -0700

    python step

--- cubes.py
+++ cubes.py
@@ -6,10 +6,12 @@ Date: 6/26/2020
 
 import sys
 
+#function
 def cubes(i):
     j = i * i * i
     return j
 
+#starting the program and calling the function
 n = int(sys.argv[1])
 for i in range(1, n+1):
     print('%s %s' %(i ,cubes(i)))
