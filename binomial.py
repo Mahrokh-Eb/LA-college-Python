2e580a0e8d9a9a1bdaf90e0f4e984b7a5f47c82c
mahrokh <mahrokhh.ebrahimi@gmail.com>
2020-07-01 19:28:27 -0700

    python step

---
+++ binomial.py
@@ -0,0 +1,12 @@
+import random
+
+def bernoulli(p= 0.5):
+    return random.random()<p
+
+n = 6
+def binomial(n, p= 0.5):
+    heads = 0
+    for i in range(n):
+        if bernoulli(p):
+            heads +=1
+        return heads
