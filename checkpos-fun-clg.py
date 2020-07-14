6618fb02989655144e098929b38fed9c31e5fcea
mahrokh <mahrokhh.ebrahimi@gmail.com>
2020-07-13 20:19:15 -0700

    python step

---
+++ checkpos-fun-clg.py
@@ -0,0 +1,12 @@
+def checkPositive(num):
+    while(num<0):
+        num = int(input('please try + number again: '))
+    return num
+
+num1 = int(input('enter first + integer: '))
+checkPositive(num1)
+
+num2 = int(input('enter second + integer: '))
+checkPositive(num2)
+
+print('num1+ num2 = ' ,num1+ num2)
