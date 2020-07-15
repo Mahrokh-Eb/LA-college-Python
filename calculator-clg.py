1f65435585efde1c392941bb8542e1f511a17ea1
mahrokh <mahrokhh.ebrahimi@gmail.com>
2020-07-15 12:39:52 -0700

    python step

--- calculator-clg.py
+++ calculator-clg.py
@@ -17,8 +17,27 @@ def checkValidation(option):
         option = int(input('invalid number'))
     return option
 
+def addition(num1, num2):
+    return num1 + num2
+
+def Subtraction(num1, num2):
+    return num1 - num2
+
+def Multiplication(num1, num2):
+    return num1 * num2
+
 
 option = 0
 while (option !=5 ):
     option = int(input('which one? \n 1) Addition\n 2) Subtraction \n 3) Multiplication \n 4) Division \n 5) Quit calculator.py'))
     option = checkValidation(option)
+    num1 = int(input('put num1= '))
+    num2 = int(input('put num2= '))
+    if option == 1:
+        print(num1 ,'+', num2 ,'= ', addition(num1, num2))
+
+    if option == 2:
+        print(num1, '-', num2, '= ', Subtraction(num1, num2))
+
+    if option == 3:
+        print(num1, 'x', num2, '= ', Multiplication(num1, num2))
