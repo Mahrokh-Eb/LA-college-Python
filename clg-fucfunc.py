8d7f2f36ae76200c1c3c331089c420d613a6c178
mahrokh <mahrokhh.ebrahimi@gmail.com>
2020-07-15 18:28:26 -0700

    python step

--- clg-fucfunc.py
+++ clg-fucfunc.py
@@ -26,23 +26,24 @@ def main():
             'which one? \n 1) Addition\n 2) Subtraction \n 3) Multiplication \n 4) Division \n 5) Quit calculator.py'))
         option = checkValidation(option)
 
-        if option == 5:
-            print('Quit calculator.py')
-            exit()
+        if option !=5:
 
-        num1 = int(input('put num1= '))
-        num2 = int(input('put num2= '))
+            num1 = int(input('put num1= '))
+            num2 = int(input('put num2= '))
 
-        if option == 1:
-            print(num1, '+', num2, '= ', addition(num1, num2))
+            if option == 1:
+                print(num1, '+', num2, '= ', addition(num1, num2))
 
-        elif option == 2:
-            print(num1, '-', num2, '= ', Subtraction(num1, num2))
+            elif option == 2:
+                print(num1, '-', num2, '= ', Subtraction(num1, num2))
 
-        elif option == 3:
-            print(num1, 'x', num2, '= ', Multiplication(num1, num2))
+            elif option == 3:
+                print(num1, 'x', num2, '= ', Multiplication(num1, num2))
 
-        elif option == 4:
-            print(num1, '/', num2, '= ', Division(num1, num2))
+            elif option == 4:
+                print(num1, '/', num2, '= ', Division(num1, num2))
+
+            else:
+                exit()
 
 main()
