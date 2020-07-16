07fa2222757f64c8bf05fea30ee7d5c08819bc33
mahrokh <mahrokhh.ebrahimi@gmail.com>
2020-07-15 20:50:37 -0700

    python step

---
+++ prac9-clg.py
@@ -0,0 +1,16 @@
+'''Define a function is_palindrome() that recognizes palindromes
+ (i.e. words that look the same written backwards).
+ For example, is_palindrome("radar") should return True.'''
+
+#def is_palindrome(word):
+
+firstName = 'MAhrokh'
+for i in firstName:
+    print(i)
+
+lastName = 'Ebrahimi'
+for i in range(len(lastName)):
+    print(lastName[i])
+
+
+
---
+++ sameForLoop.py
@@ -0,0 +1,12 @@
+# getting same result,
+# writting for loop in two ways:
+
+firstName = 'MAhrokh'
+for i in firstName:
+    print(i)
+
+print('----------------')
+
+lastName = 'Ebrahimi'
+for i in range(len(lastName)):
+    print(lastName[i])
