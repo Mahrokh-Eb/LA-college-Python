510663d120671043d949e46f2981c068f9948aae
mahrokh <mahrokhh.ebrahimi@gmail.com>
2020-07-16 14:02:37 -0700

    python step

---
+++ prac9-maxFunc.py
@@ -0,0 +1,19 @@
+# finding maximum digit, using a function
+
+def findMax(mylist):
+    maxItem = mylist[0]
+    for i in range(len(mylist)):
+        if mylist[i] > maxItem:
+            maxItem = mylist[i]
+    print('max of the list is = ',maxItem)
+    return mylist
+
+
+mylist1 = [22, 33, 44, 55, 666, 7777]
+findMax(mylist1)
+
+mylist2 = [8, 88, 2384, 1975, -32]
+findMax(mylist2)
+
+
+
