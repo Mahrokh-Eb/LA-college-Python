42d9944bae5502bdf65b267e532b83f9bace4005
mahrokh <mahrokhh.ebrahimi@gmail.com>
2020-07-19 15:19:20 -0700

    python step

--- prjct-clg-3-q2.py
+++ prjct-clg-3-q2.py
@@ -1,4 +1,4 @@
-'''# it asks the user how many students exist
+# it asks the user how many students exist
 while True:
     try:
         studentsNum= int(input('how many students do you want to enter their names? it should be a digit number: '))
@@ -16,44 +16,36 @@ print(esm)
 # it gets all student's score and save in "scores"
 scores = []
 for i in range(studentsNum):
-    for j in range(5):
+    for j in range(3):
         score = [int(input('What is the score of 5 exams for the students in the order you entered their names? ' ))]
         scores += score
     print(scores)
-'''
-'''# printing student's name and score in a matrix for esm and scores
+
+# printing student's name and score in a matrix for esm and scores
 print('studentName   ex1 ex2 ex3 ex4 ex5 \n===================================')
-for i in range(len(esm)):
-    print(esm[i])
-    for j in range(5):
-        print(scores[j])'''
+print(esm)
+print(scores)
 
+# find and return the lowest of the 5 scores passed to it
+mini = min(scores)
+maxi = 1
+for i in scores:
+    if i < mini:
+        mini = i
+    if i > maxi:
+        maxi = i
+print('minimum score is = ',mini)
+print('maximum score is = ', maxi)
 
-'''for i in range(studentsNum):
-    name = input('student name? ')
+# dropping the highest and lowest scores the student received.
+minIndx = scores.index(mini)
+maxIndx = scores.index(maxi)
+scores.remove(mini)
+scores.remove(maxi)
+print('min scores after removing minimumm and maximum scores: ',scores)
 
-    for j in range(3):
-        score = [int(input('What is the score of the exams? '))]
-    print(name, score)'''
-
-# A basic code for matrix input from user
-
-R = int(input("Enter the number of students (rows):"))
-C = 5
-
-# Initialize matrix
-matrix = []
-print("Enter the entries rowwise:")
-
-# For user input
-for i in range(R):  # A for loop for row entries
-      a = []
-      for j in range(C):  # A for loop for column entries
-            a.append(int(input('put score of exams: ')))
-      matrix.append(a)
-
-# For printing the matrix
-for i in range(R):
-      for j in range(C):
-            print(matrix[i][j], end=" ")
-      print()
+# the average of the scores that remain
+for i in scores:
+    sumofAll = sum(scores)
+    average = sumofAll / len(scores)
+print('the average is: ', average)
