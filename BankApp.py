95c174fed09dc047cc793cb01191c3b221b69e63
mahrokh <mahrokhh.ebrahimi@gmail.com>
2020-07-24 13:17:32 -0700

    python step

--- BankApp.py
+++ BankApp.py
@@ -16,29 +16,28 @@ for i in fileRead:
 
     for t in range(2, len(myList)):
         balances.append(float(myList[t]))
-def menue():
+def menue(indexBalance):
     option = input('Type D to deposit money\nType W to withdraw money\nType B to display Balance\nType C to change user, display user name\nType E to exit: ---')
-    if (option==D):
+    if (option=='D'):
         depositAmount = int(input('what is the amount of deposit? '))
-        DepositFunction(depositAmount)
+        print(indexBalance)
+        DepositFunction(depositAmount, indexBalance)
 
-def DepositFunction(depositAmount):
-#index e user o pass ro dari,
-# vhamun index male balance e. uno update kon
-# project2, program1, mige keep displaying menue untill user bege exit
+def DepositFunction(depositAmount, indexBalance):
+    balanceChange = balances[indexBalance]
+    balanceChange += depositAmount
+    print('new balance after deposit is: ',balanceChange)
+
 
 
 #print(userName[0:1]) it gives mike
 print(userName)
 print(passWord)
+print(balances)
 # username ro az user begir age tu liste username hat bud, index un ro dar are
 # pass ro az user begir, ba passe marbut be un user compare kon,
 # passe marbut b un ro az indexesh mifahmi.
 # age index e user 1, e. ba pass index i compare kon
-
-#list2 = ['cat', 'bat', 'mat', 'cat', 'pet']
-# Will print the index of 'cat' in list2
-#print(list2.index('cat'))
 
 
 while True:
@@ -50,12 +49,14 @@ while True:
     for i in userName:
         if i == user:
             indexUser = userName.index(i)
+            indexBalance = userName.index(i)
+            print(indexBalance)
             for j in passWord:
                 if j == userPass:
                     indexPass = passWord.index(j)
                     if indexUser == indexPass:
                         print('welcome Beauty!')
-                        menue()
+                        menue(indexBalance)
 
 
 
