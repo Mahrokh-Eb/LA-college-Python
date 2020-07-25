dc4712e988a8c4bad82e4cc7d8213729c0cd711e
mahrokh <mahrokhh.ebrahimi@gmail.com>
2020-07-25 12:47:08 -0700

    python step

--- BankApp.py
+++ BankApp.py
@@ -24,23 +24,24 @@ for i in fileRead:
     for t in range(2, len(myList)):
         balances.append(float(myList[t]))
 
+#function to ask user what he wants to do
 def menue(indexBalance):
     option = 'A'
     while(option != 'D' or 'W' or 'B' or 'C' or 'E' ):
         option = input('Type D to deposit money\nType W to withdraw money\nType B to display Balance\nType C to change user, display user name\nType E to exit: ---')
-        if (option == 'D'):
+        if (option == 'D'): #deposit
             depositAmount = int(input('what is the amount of deposit? '))
             validateUserInput(depositAmount) #calling function to validate user input to make sure that it is not negetive
             DepositFunction(depositAmount, indexBalance) #calling function to deposite the function and show new balance
-        elif (option == 'W'):
+        elif (option == 'W'): #withdraw
             withDrawAmount = int(input('what is the amount of money that you would like to withdraw? '))
             validateUserInput(withDrawAmount)
             withDrawFunction(withDrawAmount, indexBalance)
-        elif (option == 'B'):
+        elif (option == 'B'): #display the balance
             ShowBalanceFunction()
-        elif (option == 'C'):
+        elif (option == 'C'): #change the user
             ChangeUserFunction()
-        elif (option == 'E'):
+        elif (option == 'E'): #exit
             exit('Goodbye :)')
 
 #function to validate the option
@@ -48,16 +49,20 @@ def validateOption(option):
     while(option != 'D' or 'W' or 'B' or 'C' or 'E' ):
         option = int(input('option is invalid! '))
     return option
+
 #function to check the input that is entered by user to make sure that t is not negetive
 def validateUserInput(depositAmount):
     while (depositAmount<0):
         depositAmount = int(input('try again: '))
     return depositAmount
+
 #function to deposit the money in the account
 def DepositFunction(depositAmount, indexBalance):
+    global balanceChange
     balanceChange = balances[indexBalance]
     balanceChange += depositAmount
     print('new balance after deposit is: ',balanceChange)
+
 #function to withdraw money from the account
 def withDrawFunction(withDrawAmount, indexBalance):
     global balanceChange
@@ -65,21 +70,24 @@ def withDrawFunction(withDrawAmount, indexBalance):
     if (balanceChange > withDrawAmount):
         balanceChange -= withDrawAmount
         print('new balance after whthdrawing ',withDrawAmount, 'is= ', balanceChange )
+
 #function to display the balance
 def ShowBalanceFunction():
-    balanceChange = balances[indexBalance]
+    global balanceChange
+    #balanceChange = balances[indexBalance]
     print('the balance is= ', balanceChange)
+
 #function to change the user
 def ChangeUserFunction():
     main()
 
-print(userName)
+'''print(userName)
 print(passWord)
-print(balances)
+print(balances)'''
 
+# getting username and password from the user
 def main():
     global indexBalance
-    # getting username and password from the user
     while True:
         user = input('please Enter your userName: ')
         user = user.strip()
@@ -95,6 +103,8 @@ def main():
                     if j == userPass:
                         indexPass = passWord.index(j)
                         if indexUser == indexPass:
+                            global balanceChange
+                            balanceChange = balances[indexBalance]
                             print('welcome Beauty!')
                             menue(indexBalance)
 main()
